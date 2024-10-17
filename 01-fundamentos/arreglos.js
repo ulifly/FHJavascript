ce4b13056fb4aefc70378d32a8c279618c7df1c6
Ulises Desentis <ulifly@MacBook-Air-de-Ulises.local>
2024-10-16 18:36:50 -0600

    s3 v7

--- 01-fundamentos/arreglos.js
+++ 01-fundamentos/arreglos.js
@@ -6,3 +6,19 @@ console.log(arr2);
 let videoJuegos = ['Mario 3', 'Megaman', 'Chrono Trigger'];
 console.log({ videoJuegos });
 console.log(videoJuegos[0]);
+
+let arregloCosas = [
+    true,
+    123,
+    'Fernando',
+    1 + 2,
+    function(){},
+    ()=>{},
+    { a: 1 },
+    ['X', 'Megaman', 'Zero', 'Dr. Light', [
+        'Dr. Willy',
+        'Woodman'
+    ]]
+];
+
+console.log(arregloCosas[7][4][1]);
---
+++ 01-fundamentos/arreglos2.js
@@ -0,0 +1,3 @@
+let videoJuegos = ['Mario 3', 'Megaman', 'Chrono Trigger', 'Zelda'];
+console.log('Largo:', videoJuegos.length);
+
